bf2b110dd4320751897e9711a1d723a688249e95
HagbardCeline <janpauldahlke@gmail.com>
2022-11-09 09:40:51 +0100

    handles window size

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,19 @@
 
 use bevy::prelude::*;
 
-// region: -- Asset constants
-// --player
+// region: --- Asset constants
 const PLAYER_SPRITE: &str = "black_jesus.png";
-const PLAYER_SIZE: (f32, f32) = (144., 75.);
-
-// --enemy
+const PLAYER_SIZE: (f32, f32) = (144., 177.);
+const SPRITE_SCALE: f32 = 0.5;
 const ENEMY_SPRITE: &str = "enemy_a_01.png";
-// endregion: -- Asset constants
+// endregion: --- Asset constants
+
+// region: --- Resources
+pub struct WinSize {
+    pub w: f32,
+    pub h: f32,
+}
+// endregion: --- Resource
 
 fn main() {
     App::new()
@@ -38,14 +43,18 @@ fn setup_system(
     let (win_w, win_h) = (window.width(), window.height());
 
     //position window
-    window.set_position(IVec2::new(1920, 1080));
+    window.set_position(IVec2::new(2780, 4900));
+    // size window
+    let win_size = WinSize { w: win_w, h: win_h };
+    commands.insert_resource(win_size);
 
     // - add player
     let bottom = -win_h / 2.;
     commands.spawn_bundle(SpriteBundle {
         texture: asset_server.load(PLAYER_SPRITE),
         transform: Transform {
-            translation: Vec3::new(0., bottom + PLAYER_SIZE.1 / 2. + 5., 10.),
+            translation: Vec3::new(0., bottom + PLAYER_SIZE.1 / 2. * SPRITE_SCALE + 5., 10.),
+            scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
             ..Default::default()
         },
         ..Default::default()
