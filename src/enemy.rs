e4f6d4d733f0a1b49e7fce192d84a3bdec228150
HagbardCeline <janpauldahlke@gmail.com>
2022-11-11 13:57:51 +0100

    we need a pause here

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,5 +1,6 @@
-use crate::GameTextures;
+use crate::{GameTextures, WinSize, SPRITE_SCALE};
 use bevy::{prelude::*, transform};
+use rand::{thread_rng, Rng};
 
 pub struct EnemyPlugin;
 
@@ -9,9 +10,26 @@ impl Plugin for EnemyPlugin {
     }
 }
 
-fn enemy_spawn_system(mut commands: Commands, game_textures: Res<GameTextures>) {
+fn enemy_spawn_system(
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+    win_size: Res<WinSize>,
+) {
+    // compute random position
+    let mut rng = thread_rng();
+    let w_span = win_size.w / 2. - 100.;
+    let h_span = win_size.h / 2. - 100.;
+
+    let x = rng.gen_range(-w_span..w_span);
+    let y = rng.gen_range(-h_span..h_span);
+
     commands.spawn_bundle(SpriteBundle {
         texture: game_textures.enemy.clone(),
+        transform: Transform {
+            translation: Vec3::new(x, y, 10.),
+            scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+            ..Default::default()
+        },
         ..Default::default()
     });
 }
